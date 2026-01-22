6a143b2b9dfa0890b60a6eedfd81a80973b7cde8
Gab <gbrlferz@gmail.com>
2026-01-22 17:12:10 -0300

    Day 1

---
+++ misc/shell.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+set path=C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE;%path%
+set path=w:\handmade\misc;%path%;
